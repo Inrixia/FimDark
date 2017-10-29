6710f17bf3a8de75766e265bacd03f10008df42e
Inrixia <Inrixia@gmail.com>
2017-10-29 17:27:04 +1300

    Imagefix

--- FimBanner.user.js
+++ FimBanner.user.js
@@ -19,7 +19,7 @@
         //Uncomment lines 18 and 27 to Only show banner on the homepage. Everything else is default
         //if (window.location.pathname == '/'){
         //-------------------------------------------------------------------------------------------vv Here is the url that you need to change to change the picture!
-        $(".page-nav-bars").append('<div><a href="/" class="home_link" style="background-image: url(https://github.com/Inrixia/inrixia.github.io/raw/master/MiPpYJx.jpg);width: 100%;position: absolute;height: 300px;background-position: center top;background-size: cover;"></a><div style="height: 260px;"></div></div>');
+        $(".page-nav-bars").append('<div><a href="/" class="home_link" style="background-image: url(https://github.com/Inrixia/inrixia.github.io/raw/master/REPLACE_THIS_URL.png);width: 100%;position: absolute;height: 300px;background-position: center top;background-size: cover;"></a><div style="height: 260px;"></div></div>');
         //-------------------------------------------------------------------------------------------^^ Here is the url that you need to change to change the picture!
 
         $(".user_toolbar").css("background", "linear-gradient(rgba(146, 27, 87, 0) 0%, rgb(146, 27, 87) 85%)").css("position", "relative").css("border-bottom", "none").css("box-shadow", "none");
