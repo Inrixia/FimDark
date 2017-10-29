f292bdee315148809962d5d1cd808212778aa868
Inrixia <Inrixia@gmail.com>
2017-10-29 20:03:06 +1300

    Update darkmode.js

--- darkmode.js
+++ darkmode.js
@@ -1,3 +1,7 @@
 (function() {
-    $("head").append('<link rel="stylesheet" href="https://inrixia.github.io/darkmode.css" type="text/css">');
+    $("head").append('<link id="first_dark" rel="stylesheet" href="https://inrixia.github.io/darkmode.css" type="text/css">');
+    window.onload = function(){
+        $("head").append('<link rel="stylesheet" href="https://inrixia.github.io/darkmode.css" type="text/css">');
+        $("#first_dark").remove();
+    };
 })();
