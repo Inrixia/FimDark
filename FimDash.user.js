b4ab4b62199edd9b011fac8e7f0e671aca7e6849
Inrixia <Inrixia@gmail.com>
2017-10-29 20:07:17 +1300

    Update FimDash.user.js

--- FimDash.user.js
+++ FimDash.user.js
@@ -1,19 +1,22 @@
 // ==UserScript==
 // @name         Fim Dash
 // @namespace    http://tampermonkey.net/
-// @version      1.3
+// @version      1.2
 // @description  Time to take over FimFiction
 // @author       YourTrueOverlord
 // @match        http://tampermonkey.net/index.php?version=4.4&ext=dhdg&updated=true
 // @grant        none
 // @run-at document-start
 // @require http://code.jquery.com/jquery-1.12.4.min.js
-// @require https://github.com/Inrixia/inrixia.github.io/raw/master/darkmode.js
 // @include http://www.fimfiction.net/*
 // @include https://www.fimfiction.net/*
 // ==/UserScript==
 
 
 (function() {
-  
+    $("head").append('<link id="first_dark" rel="stylesheet" href="https://inrixia.github.io/darkmode.css" type="text/css">');
+    window.onload = function(){
+        $("head").append('<link rel="stylesheet" href="https://inrixia.github.io/darkmode.css" type="text/css">');
+        $("#first_dark").remove();
+    };
 })();
