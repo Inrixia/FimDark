70f9425a8491a8fe0d8310ee620dbb62a100f4c1
Inrixia <Inrixia@gmail.com>
2017-10-29 20:11:07 +1300

    Delete darkmode.js

--- darkmode.js
+++
@@ -1,7 +0,0 @@
-(function() {
-    $("head").append('<link id="first_dark" rel="stylesheet" href="https://inrixia.github.io/darkmode.css" type="text/css">');
-    window.onload = function(){
-        $("head").append('<link rel="stylesheet" href="https://inrixia.github.io/darkmode.css" type="text/css">');
-        $("#first_dark").remove();
-    };
-})();
